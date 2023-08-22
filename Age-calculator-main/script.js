e30852cbc77ab9e7449055188c033f590b249e4c
MokhammadVu <stechkin_95@hotmail.com>
2023-08-22 12:08:04 +0200

    up

--- Age-calculator-main/script.js
+++ Age-calculator-main/script.js
@@ -10,12 +10,13 @@ inputMonth.addEventListener("input", update);
 
 // Function to calculate and update the countdown between the selected date and today.
 
-// Get the values from the input fields
-
 function update() {
   const results = document.querySelector(".result");
+  const resultsPlus = document.querySelector(".results_plus");
   results.style.display = "flex";
+  resultsPlus.style.display = "grid";
 
+  // Obtain values from the input fields
   let month = inputMonth.value - 1; // Months in JavaScript's Date object are 0 index
   let day = inputDay.value;
   let year = inputYear.value;
@@ -30,7 +31,6 @@ function update() {
   const callDate = today.getTime() - selectedDate.getTime();
 
   // Calculate the years, months, and days difference
-
   let years = Math.floor(callDate / (1000 * 60 * 60 * 24 * 365.25));
   let months = Math.floor(
     (callDate % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44)
@@ -47,33 +47,54 @@ function update() {
 
   // Add "days", "months", and "years" labels to the displayed countdown values
 
-  const daySpan = document.createElement("span");
-  daySpan.textContent = "days";
+  const daySpan = createSpan("days");
   document.getElementById("countd").append(daySpan);
 
-  const monthSpan = document.createElement("span");
-  monthSpan.textContent = "months";
+  const monthSpan = createSpan("months");
   document.getElementById("countm").append(monthSpan);
 
-  const yearSpan = document.createElement("span");
-  yearSpan.textContent = "years";
+  const yearSpan = createSpan("years");
   document.getElementById("county").append(yearSpan);
 
-  const error = ageError();
+  // Function to format time units and update their display
 
-  // Обработка ошибок
-  if (error) {
-    // Делайте здесь что-то с сообщением об ошибке, например, выводите на экран
-    console.error(error);
+  function formatTimeUnits() {
+    const totalMonths = years * 12 + months;
+    const totalWeeks = Math.floor((years * 365) / 7);
+    const totalDays = Math.floor(totalWeeks * 7);
+    const hours = totalDays * 24;
+    const minutes = hours * 60;
+    const seconds = minutes * 60;
+
+    document.getElementById("countmplus").textContent = `${totalMonths}`;
+    const totalmonthsSpan = document.createElement("span");
+    totalmonthsSpan.textContent = "months";
+    document.getElementById("countmplus").append(totalmonthsSpan);
+
+    // Define an array of units containing their IDs, values, and labels
+
+    const units = [
+      { id: "countmplus", value: totalMonths, label: "months" },
+      { id: "countwplus", value: totalWeeks, label: "weeks" },
+      { id: "countdplus", value: totalDays, label: "days" },
+      { id: "counthplus", value: hours, label: "hours" },
+      { id: "countmiplus", value: minutes, label: "minutes" },
+      { id: "countsplus", value: seconds, label: "seconds" },
+    ];
+
+    // Iterate through each unit in the array and update the corresponding HTML element
+    units.forEach((unit) => {
+      const element = document.getElementById(unit.id);
+      element.textContent = `${unit.value.toLocaleString()} ${unit.label}`;
+      element.append(createSpan(unit.label));
+    });
   }
+
+  formatTimeUnits();
 }
 
-function ageError() {
-  if (inputDay.value < 1 || inputDay.value > 31) {
-    return "Must be a valid day";
-  } else if (inputMonth.value < 1 || inputMonth.value > 12) {
-    return "Must be a valid month";
-  } else if (inputYear.value < 1900 || inputYear.value > 2023) {
-    return "Must be a valid year";
-  }
+function createSpan(text) {
+  const span = document.createElement("span");
+  span.textContent = text;
+  return span;
 }
